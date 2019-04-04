da87b0103c56a2e007bf36a378e160ab392d8135
codemzy <codemzy@gmail.com>
2019-04-04 14:31:19 +0000

    Refactored Login component to use new Form component

--- src/app/components/login/Login.js
+++ src/app/components/login/Login.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { appPath } from '../../config/settings';
 
 // hoc
 import withHead from '../hocHead';
 
-//components
-import Main from './Main';
-import LoginForm from './Login_Form';
+// context
+import { getContext } from '../../context';
 
+// api calls
+import { userLogin } from '../../api/auth';
+
+// components
+import Main from './Main';
+import Form from './Form';
+
+// log in component - displays and handles log in form
 function Login(props) {
+  
+  // state hook
+  const [loading, setLoading] = useState(false);
+  
+  // submit the form
+  const handleSubmit = function(form) {
+    setLoading(true);
+    userLogin(form.email, form.password).catch((error) => {
+      setLoading(false);
+      props.alertContext.updateAlert({ type: "danger", message: error.message });
+    });
+  }
+  
   return (
     <Main title="Log In" footer={<small><p className="text-white">Don't have an account? <Link to={appPath + "/register"} className="text-white"><u>Register</u></Link></p></small>}>
-      <LoginForm />
+      <h6 className="text-muted">Welcome back! Please enter your email address and password.</h6>
+      <Form 
+        loading={loading} 
+        handleSubmit={handleSubmit}
+        email={true} password={true} 
+        button={{submit: "Log In", submitted: "Logging In"}} />
     </Main>
   );
 }
 
-export default withHead(Login, "Log In");
-
+export default getContext({
+  alert
+})(withHead(Login, "Log In"));
